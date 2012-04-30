0e38849fc839ba0966e6b65b9670a243f5bbc98b
Bartek Przybylski <bart.p.pl@gmail.com>
2012-04-30 19:37:58 +0200

    initial commit

---
+++ Contacts/src/com/owncloud/contacts/ContactsSyncAdapter.java
@@ -0,0 +1,39 @@
+/* ownCloud Android client application
+ *   Copyright (C) 2012  ownCloud
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.owncloud.contacts;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+public class ContactsSyncAdapter extends AbstractThreadedSyncAdapter {
+
+  public ContactsSyncAdapter(Context context, boolean autoInitialize) {
+    super(context, autoInitialize);
+  }
+
+  @Override
+  public void onPerformSync(Account account, Bundle bundle, String authority,
+      ContentProviderClient provider, SyncResult syncResult) {
+  }
+
+}
---
+++ Contacts/src/com/owncloud/contacts/ownCloudContactsActivity.java
@@ -0,0 +1,16 @@
+package com.owncloud.contacts;
+
+import com.owncloud.R;
+import com.owncloud.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ownCloudContactsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
